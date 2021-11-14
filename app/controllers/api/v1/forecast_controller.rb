4629438b2b6e0d00a88aafd56b221bfed7feb4fc
Rteske <ryanteske@outlook.com>
2021-11-13 18:55:58 -0800

    Add request show for forecast controller

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ForecastController < ApplicationController
   def show
-    stuff = ForecastFacade.forecast(params)
-    render json: stuff
+    forecast = Api::V1::ForecastFacade.forecast(params)
+    render json: { data: { id: 'null', type: 'forecast', attributes: forecast } }
   end
 end
