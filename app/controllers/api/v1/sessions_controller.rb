8f30c78115ee30adee3ebf84c0094fc7171a64d8
Rteske <ryanteske@outlook.com>
2021-11-16 08:54:13 -0800

    Add post sessions request w/ tests, routes and controller

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: session_params[:email])
+
+    if user.authenticate(session_params[:password])
+      user.api_key = user.api_keys.first.token
+      render json: Api::V1::UserSerializer.new(user)
+    else
+      render json: 'bad credentials', status: 401
+    end
+  end
+
+  private
+
+  def session_params
+    params.permit(:email, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       get '/activities', to: 'activities#index'
       get '/backgrounds', to: 'backgrounds#index'
       post '/users', to: 'users#create'
+      post '/sessions', to: 'sessions#create'
     end
   end
 end
